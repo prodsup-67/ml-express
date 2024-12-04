22599a2f661e2b6eee40976fb071238363b0c587
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-12-05 06:44:09 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import cors from "cors";
 import debug from "debug";
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 import multer from "multer";
 
 import { loadModel, predict, readImageEncoded, readImageFile } from "./ml.js";
@@ -21,30 +21,59 @@ app.get("/", (req, res) => {
   res.json({ hello: "world" });
 });
 
-app.get("/load", async (req, res) => {
-  const model = await loadModel();
-  res.json({ model });
+app.get("/load", async (req, res, next) => {
+  try {
+    const model = await loadModel();
+    res.json({ model });
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.post("/upload", upload.single("test"), async (req, res, next) => {
-  const { model, classes } = await loadModel();
-  const contentType = req.file?.mimetype ?? "";
-  const filePath = req.file?.path ?? "";
-  const imageBitmap = await readImageFile(filePath, contentType);
-  const predictions = await predict(imageBitmap, model, classes);
-  res.json({ predictions });
+app.post("/upload", upload.single("img"), async (req, res, next) => {
+  try {
+    const { model, classes } = await loadModel();
+    const contentType = req.file?.mimetype ?? "";
+    const filePath = req.file?.path ?? "";
+    const imageBitmap = await readImageFile(filePath, contentType);
+    const predictions = await predict(imageBitmap, model, classes);
+    res.json({ predictions });
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.post("/upload_base64", async (req, res, next) => {
-  const { model, classes } = await loadModel();
-  const imageEncoded = req.body.imageEncoded ?? "";
-  const imageBitmap = await readImageEncoded(imageEncoded);
-  const predictions = await predict(imageBitmap, model, classes);
-  logger(predictions);
-  res.json({ predictions });
+  try {
+    const { model, classes } = await loadModel();
+    const imageEncoded = req.body.imageEncoded ?? "";
+    const imageBitmap = await readImageEncoded(imageEncoded);
+    const predictions = await predict(imageBitmap, model, classes);
+    logger(predictions);
+    res.json({ predictions });
+  } catch (err) {
+    next(err);
+  }
 });
 
-// * Running app
+// JSON Error Middleware
+const jsonErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let serializedError = JSON.stringify(err, Object.getOwnPropertyNames(err));
+  serializedError = serializedError.replace(/\/+/g, "/");
+  serializedError = serializedError.replace(/\\+/g, "/");
+  res.status(500).send({
+    error: serializedError,
+    predictions: [
+      {
+        class: "ERROR",
+        score: 1,
+      },
+    ],
+  });
+};
+app.use(jsonErrorHandler);
+
+// Running app
 app.listen(PORT, async () => {
   logger(`Listening on port ${PORT}: http://localhost:${PORT}`);
 });
